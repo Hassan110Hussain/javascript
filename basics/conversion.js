2f08d55ae15ac322becffafb2b3452ab788c2331
Hassan110Hussain <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-09 15:18:35 +0000

    Add conversion.js with examples of type conversion and console logs

---
+++ basics/conversion.js
@@ -0,0 +1,28 @@
+let age = "32hm";
+
+console.log(typeof age);
+console.log(typeof age);
+
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// '32' => 32
+// '32abc' => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false , "hassan" => true
+
+let someNumber = 34;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
