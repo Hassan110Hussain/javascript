911b8666dc7a42c62c7e5659bb4207a68373a2e2
Hassan110Hussain <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-09 14:56:15 +0000

    Add dataTypes.js with basic data type examples and console logs

---
+++ basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as modern
+
+// alert('Hello World!'); // we r using nodejs and not browser
+
+let name = "Hassan";
+let age = 23;
+let isApproved = false;
+
+// number => 323
+// float => 3.234
+// bigint
+// string => '' or ""
+// boolean => true or false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
