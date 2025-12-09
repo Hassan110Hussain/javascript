f59ff2ca083a8784f324c0c9ed84752b05b7ecee
Hassan110Hussain <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-09 15:55:29 +0000

    Add conversion and operations examples with console logs

---
+++ basics/operations.js
@@ -0,0 +1,31 @@
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = " hassan";
+
+let fullStr = str1 + str2;
+
+console.log(fullStr);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(((3 + 4) * 5) % 4); // use more parenthesis
+
+console.log(+true);
+console.log(+""); // not preferred on production level
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
