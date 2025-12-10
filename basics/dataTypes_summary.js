b81b35ab9e3feb6386a6b0be3f386ea2660ef956
Hassan110Hussain <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-10 08:56:25 +0000

    Add dataTypes_summary.js with descriptions of primitive and reference data types

---
+++ basics/dataTypes_summary.js
@@ -0,0 +1,9 @@
+// Primitive Data Types in JavaScript
+
+// 7 types : String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+
+
+// Reference Data Types in JavaScript (Non-primitive)
+
+// Array, Objects, Functions
