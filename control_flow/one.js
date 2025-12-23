176c76b88714cc65fb4a958b02e81779075a0ad4
Hassan Hussain Merchant <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-23 12:00:26 +0000

    Add one.js with examples of control flow and conditional statements

---
+++ control_flow/one.js
@@ -0,0 +1,20 @@
+//  if
+const temp = 27;
+
+// if (temp < 20) {
+//   console.log("century drought");
+// } else {
+//   console.log("century done");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+if (2 !== "2") {
+  // console.log('executed');
+}
+
+const score = 200
+
+if (score > 100) {
+    const power = 'fly'
+    console.log(`user power: ${power}`);
+}
