808b86df6e69e05f9e78c3b3381d2881fed3b25a
Hassan Hussain Merchant <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-24 11:26:00 +0000

    Refactor one.js to improve code formatting and clarity

--- control_flow/one.js
+++ control_flow/one.js
@@ -12,25 +12,40 @@ if (2 !== "2") {
   // console.log('executed');
 }
 
-const score = 200
+const score = 200;
 
 if (score > 100) {
-    const power = 'fly'
-    // console.log(`user power: ${power}`);
+  const power = "fly";
+  // console.log(`user power: ${power}`);
 }
 
 // console.log(`user power: ${power}`);
 
-const balance = 1000
+const balance = 1000;
 
 // if (balance > 500) console.log('test'), console.log('test2');  //  not professional, use scope {}
 
-if (balance < 500) {
-  console.log('yes its a two');
-} else if (balance < 750) {
-  console.log('yes its a two');
-} else if (balance < 1000) {
-  console.log('yes its a two');
-} else {
-  console.log('ye agya range mai');
+// if (balance < 500) {
+//   console.log('yes its a two');
+// } else if (balance < 750) {
+//   console.log('yes its a two');
+// } else if (balance < 1000) {
+//   console.log('yes its a two');
+// } else {
+//   console.log('ye agya range mai');
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const LoggedInGoogle = false;
+const LoggedInEmail = false;
+
+// && all the statements must be true, if only one statment is false then it wont go in if condition
+if (userLoggedIn && debitCard && LoggedInGoogle) {
+  console.log("allow to buy course");
+}
+
+// || used for checking multiple conditions, only one have to be true 
+if (LoggedInGoogle || LoggedInEmail || debitCard) {
+  console.log("login allowed");
 }
