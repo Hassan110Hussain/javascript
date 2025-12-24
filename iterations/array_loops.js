4900b27762f938cc985cdb9ce3976a0aa6c03976
Hassan Hussain Merchant <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-24 15:25:29 +0000

    Add array_loops.js with examples of for-of loops and maps

---
+++ iterations/array_loops.js
@@ -0,0 +1,39 @@
+// for of
+
+// ['', '', '']
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5];
+
+// for (const i of arr) {
+//   console.log(i);
+// }
+
+const greetings = "hello boy";
+
+for (const greet of greetings) {
+  // console.log(greet);
+}
+
+// maps
+
+const map = new Map();
+map.set("name", "Hassan");
+map.set("name2", "Haider");
+map.set("name3", "Hussain");
+
+// console.log(map);
+
+for (const [key, value] of map) {
+  console.log(`${key}: ${value}`);
+}
+
+const myObject = {
+  name: "Hassan",
+  age: 25,
+  city: "Karachi",
+}
+
+// for (const [key, value] of myObject) {
+//   console.log(`${key}: ${value}`);
+// }  //  not working on objects
