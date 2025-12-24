2cf3b1d9c037e23b40a5ea553ba8b6895363b869
Hassan Hussain Merchant <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-24 14:18:54 +0000

    Refactor one.js to comment out console logs and improve code clarity

--- iterations/one.js
+++ iterations/one.js
@@ -1,9 +1,42 @@
 // for
 
 for (let i = 0; i < 10; i++) {
-    const ele = i;
-    if (ele === 5) {
-        console.log('5 is best number');
-    }
-    console.log(ele);
+  const ele = i;
+  if (ele === 5) {
+    // console.log('5 is best number');
+  }
+  // console.log(ele);
+}
+
+for (let i = 0; i <= 2; i++) {
+  // console.log(`inner loop ${i}`);
+  for (let j = 0; j <= 2; j++) {
+    // console.log(`inner loop ${j} and ${i}`);
+    // console.log(i + '*' +  j + '=' + i*j);
+  }
+}
+
+let myArray = ["thor", "superman", "loki"];
+
+for (i = 0; i < myArray.length; i++) {
+  const element = myArray[i];
+  // console.log(element);
+}
+
+// break n continue
+
+for (let i = 0; i < 20; i++) {
+  if (i == 5) {
+    // console.log('5 detected');
+    break;
+  }
+  // console.log(`${i}`);
+}
+
+for (let i = 0; i < 10; i++) {
+  if (i == 5) {
+    console.log("5 detected");
+    continue;
+  }
+  console.log(`${i}`);
 }
