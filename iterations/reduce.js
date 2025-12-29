84a3f23589312a6bcb6338073659f77675464c97
Hassan Hussain Merchant <166353200+Hassan110Hussain@users.noreply.github.com>
2025-12-29 12:57:17 +0000

    Add reduce.js example demonstrating array reduction and total calculation

--- iterations/reduce.js
+++ iterations/reduce.js
@@ -1,2 +1,38 @@
 // reduce
 const myNums = [1, 2, 3];
+
+// const myTotal = myNums.reduce(function (acc, curval) {
+//     console.log(`${acc} & ${curval}`);
+//     return acc + curval
+// }, 0)
+
+const myTotal = myNums.reduce((acc, curval) => {
+  return acc + curval;
+}, 0);
+
+// console.log(myTotal);
+
+const shoppingCart = [
+  {
+    itemName: "js course",
+    price: 2999,
+  },
+  {
+    itemName: "py course",
+    price: 1999,
+  },
+  {
+    itemName: "mobile dev course",
+    price: 4999,
+  },
+  {
+    itemName: "data course",
+    price: 3999,
+  },
+];
+
+const shoppingList = shoppingCart.reduce((acc, item) => {
+  return acc + item.price;
+}, 0);
+
+console.log(shoppingList);
