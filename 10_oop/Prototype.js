ae5c543436fc2b8030ecfb046810bcafe2def6cc
Hassan110Hussain <merchanthassan34@gmail.com>
2026-02-17 17:13:08 +0500

    Update ApiRequest.html to replace outdated examples with modern Fetch API usage, enhancing clarity and relevance for current best practices.

---
+++ 10_oop/Prototype.js
@@ -0,0 +1,18 @@
+// let myName = 'Hassan       '
+// let myName2 = 'Cricket     '
+
+// console.log(myName.trueLength);
+
+let myHeroes = ["thor", "ironman", "superman"];
+
+let heroPower = {
+  thor: "hammer",
+  ironman: "iron-body",
+  superman: "super-natural",
+
+  getThorPower: function () {
+    console.log(`Thor power is ${this.thor}`);
+  },
+};
+
+heroPower.hassan();
