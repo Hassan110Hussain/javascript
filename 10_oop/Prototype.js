1024842cb998496972c16fefc737907defc786d7
Hassan110Hussain <merchanthassan34@gmail.com>
2026-02-18 12:23:50 +0500

    Add prototype methods to Object and String, implement inheritance for User and Teacher, and enhance functionality with trueLength method for strings.

--- 10_oop/Prototype.js
+++ 10_oop/Prototype.js
@@ -15,4 +15,53 @@ let heroPower = {
   },
 };
 
-heroPower.hassan();
+Object.prototype.hassan = function () {
+  console.log(`hassan is in all objects`);
+};
+
+Array.prototype.heyHassan = function () {
+  console.log(`Hassan says hello`);
+};
+
+// heroPower.hassan();
+// myHeroes.hassan()
+// heroPower.heyHassan();
+myHeroes.heyHassan();
+
+//  inheritance  //
+
+const User = {
+  name: "coffee",
+  email: "hassan@gmail.com",
+};
+
+const Teacher = {
+  makeVideo: true,
+};
+
+const TeachingSupport = {
+  isAvailable: false,
+};
+
+const TASupport = {
+  makeAssignment: "JS assignment",
+  fullTime: true,
+  __proto__: TeachingSupport,
+};
+
+Teacher.__proto__ = User;
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher);
+
+let anotherUser = "CricketnCoffee      ";
+
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`True length is: ${this.trim().length}`);
+};
+
+anotherUser.trueLength();
+"hassan".trueLength();
+"worldcup   ".trueLength();
