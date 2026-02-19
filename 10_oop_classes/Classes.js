1c2c4cbb774c464cafc12eb0cf67b275f889fbcd
Hassan110Hussain <merchanthassan34@gmail.com>
2026-02-19 09:50:55 +0500

    Refactor User class implementation to use constructor function and prototype methods, updating instance creation and method calls for improved clarity and functionality.

--- 10_oop_classes/Classes.js
+++ 10_oop_classes/Classes.js
@@ -1,17 +1,42 @@
 //  ES6
 
-class User{
-    constructor(username, email, password){
-        this.username = username,
+// class User{
+//     constructor(username, email, password){
+//         this.username = username,
+//         this.email = email,
+//         this.password = password
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// const coffee = new User('cricket', 'cricket@gmail.com', '214235')
+
+// console.log(coffee.encryptPassword());
+// console.log(coffee.changeUsername());
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username,
         this.email = email,
         this.password = password
-    }
-
-    encryptPassword(){
-        return `${this.password}abc`
-    }
 }
 
-const chai = new User('cricket', 'cricket@gmail.com', '214235')
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
 
-console.log(chai.encryptPassword());
+User.prototype.changeUsername = function(){
+     return `${this.username.toUpperCase()}`
+}
+
+const cricket = new User('coffee', 'hassan@gmail.com', '23345')
+
+console.log(cricket.encryptPassword());
+console.log(cricket.changeUsername());
