8fec6eb78580a5cef7f83b6fe25f65c01d1fda4d
Hassan110Hussain <merchanthassan34@gmail.com>
2026-02-19 10:06:57 +0500

    Refactor inheritance.js to improve code readability and modernize syntax, including consistent use of semicolons and streamlined constructor assignments in User and Teacher classes.

--- 10_oop_classes/inheritance.js
+++ 10_oop_classes/inheritance.js
@@ -1,30 +1,29 @@
 class User {
-    constructor(username){
-        this.username = username
-    }
-    logMe(){
-        console.log(`Username is ${this.username}`);
-    }
+  constructor(username) {
+    this.username = username;
+  }
+  logMe() {
+    console.log(`Username is ${this.username}`);
+  }
 }
 
 class Teacher extends User {
-    constructor(username, email, password){
-        super(username)
-        this.email = email,
-        this.password = password
-    }
-    addCourse(){
-        console.log(`A new course is added ${this.username}`);
-    }
+  constructor(username, email, password) {
+    super(username);
+    ((this.email = email), (this.password = password));
+  }
+  addCourse() {
+    console.log(`A new course is added ${this.username}`);
+  }
 }
 
-const coffee = new Teacher('coffee', 'coffee@gmail.com', '32523')
+const coffee = new Teacher("coffee", "coffee@gmail.com", "32523");
 
-coffee.addCourse()
+coffee.addCourse();
 
-const blackCoffee = new User('black coffee')
+const blackCoffee = new User("black coffee");
 
-blackCoffee.logMe()
+blackCoffee.logMe();
 
 console.log(coffee === blackCoffee);
 console.log(coffee instanceof Teacher);
